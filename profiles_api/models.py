4b39c980490797b55147f4d6f653101903d1d657
Shantanu <shivam.aryan17@gmail.com>
2021-07-08 18:57:29 +0530

    Fixing indentation error in profiles_api/models.py which was not letting creation of superuser

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -6,18 +6,19 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
-    def create_user(self, email, name, password= None):
+
+    def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
             raise ValueError('User must have an email address')
 
-            email = self.normalize_email(email)
-            user = self.model(email=email, name=name)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
 
-            user.set_password(password)         #Storing the password as hash rather than plaintext in Database
-            user.save(using=self._db)
+        user.set_password(password)         #Storing the password as hash rather than plaintext in Database
+        user.save(using=self._db)
 
-            return user
+        return user
 
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
@@ -26,6 +27,8 @@ class UserProfileManager(BaseUserManager):
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
+
+        return user
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
